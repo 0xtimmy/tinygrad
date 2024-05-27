e62b35fb9ee41b1578fce9df26df913d85c98fb6
timmy <timmy0x@proton.me>
2024-05-27 13:12:12 -0700

    smarter tc opt

--- tinygrad/codegen/kernel.py
+++ tinygrad/codegen/kernel.py
@@ -320,14 +320,14 @@ class Kernel:
 
   # ******************** high level optimizers ********************
 
-  def _apply_tc_opt(self, use_tensor_cores:int, axis:int, opt_level:int, reduceop:LazyOp) -> bool:
+  def _apply_tc_opt(self, use_tensor_cores:int, axis:int, opt_level:int) -> bool:
     if use_tensor_cores and self.opts.has_local and self.reduceop and self.reduceop.op is ReduceOps.SUM:
       for tc in self.opts.tensor_cores:
         has_cast = tc.dtype_in != tc.dtype_out
-        if has_cast and not(reduceop.src[0].op is UnaryOps.CAST and reduceop.src[0].arg == tc.dtype_out): break
+        if has_cast and all([not(reduceop.src[0].op is UnaryOps.CAST and reduceop.src[0].arg == tc.dtype_out) for reduceop in self.reduceops]): break
 
-        mul_op = reduceop.src[0].src[0] if has_cast else reduceop.src[0]
-        if mul_op.op is not BinaryOps.MUL: break
+        mul_ops = [r.src[0].src[0] for r in self.reduceops] if has_cast else [r.src[0] for r in self.reduceops]
+        if [op.op is not BinaryOps.MUL for op in mul_ops]: continue
 
         def buf_index(src: LazyOp) -> Optional[int]:
           # TODO: apply tc even if the sources are not from LOAD
@@ -336,23 +336,26 @@ class Kernel:
             if opt_level >= 1 and src.op is UnaryOps.CAST and src.arg == tc.dtype_in: return self.bufs.index(cast(MemBuffer, src.src[0].arg))
           except ValueError: return None
           return None
-        if (buf0:=buf_index(mul_op.src[0])) is None or (buf1:=buf_index(mul_op.src[1])) is None: break
+        buf_pairs = [(buf_index(op.src[0]), buf_index(op.src[1])) for op in mul_ops]
+        if any([buf0 is None or buf1 is None for (buf0, buf1) in buf_pairs]): continue
 
-        buf0_strides, buf1_strides = self.sts[buf0].real_strides(), self.sts[buf1].real_strides()
-        axis_buf0 = [(i,self.full_shape[i],buf1_strides[i]) for i,s in enumerate(buf0_strides[:self.first_reduce]) if s == 0]
-        axis_buf1 = [(i,self.full_shape[i],buf0_strides[i]) for i,s in enumerate(buf1_strides[:self.first_reduce]) if s == 0]
-        if not(axis_buf0 and axis_buf1 and ((self.shape_len-self.first_reduce) == 1 or (opt_level >= 1))): break
+        stride_pairs = [(self.sts[buf0].real_strides(), self.sts[buf1].real_strides()) for (buf0, buf1) in buf_pairs]
+        axis_pairs = [(
+          [(i,self.full_shape[i],buf1_strides[i]) for i,s in enumerate(buf0_strides[:self.first_reduce]) if s == 0],
+          [(i,self.full_shape[i],buf0_strides[i]) for i,s in enumerate(buf1_strides[:self.first_reduce]) if s == 0]
+        ) for (buf0_strides, buf1_strides) in stride_pairs]
+        if any(not(axis_buf0 and axis_buf1 and ((self.shape_len-self.first_reduce) == 1 or (opt_level >= 1))) for (axis_buf0, axis_buf1) in axis_pairs): continue
 
+        axis_buf0, axis_buf1 = axis_pairs[0]
         axis_choices = list(itertools.product(axis_buf0, axis_buf1, range(self.first_reduce, self.shape_len)))
-        if not(axis < len(axis_choices)): break
-
+        if not(axis < len(axis_choices)): continue
 
         s0, s1, s2 = axis_choices[-(axis+1)][0][0], axis_choices[-(axis+1)][1][0], axis_choices[-(axis+1)][2]  # s0 is n, s1 is m, s2 is k
         axis_pads = [(x, tc.dims[i]) for i, x in enumerate([s0, s1, s2]) if self.full_shape[x]%tc.dims[i] != 0]
         if axis_pads and (opt_level < 2): break
 
         # tensor core -- unroll the reduce dim, upcast input, then create the correct thread pattern
-        self.tensor_core_opts = (tc_opts:=TensorCoreOptions(bufs=(buf0, buf1), axes=[s0, s1], axes_exist=[True, True]))
+        self.tensor_core_opts = (tc_opts:=TensorCoreOptions(bufs=(b for pair in buf_pairs for b in pair), axes=[s0, s1], axes_exist=[True, True]))
 
         # attempt to pad the tensor axes that require it
         try:
@@ -418,7 +421,7 @@ class Kernel:
       check(len(self.applied_opts) == 0, "tensor core opts must be first") # TODO: things like PADTO might be fine
       check(opt.axis is not None and opt.amt is not None, "tensor core opts must have an axis and amt")
       check((use_tensor_cores:=getenv("TC", 1)) == 2 or len(self.opts.tensor_cores) > 0, "must have tensor cores or TC=2")
-      check(all([self._apply_tc_opt(use_tensor_cores, cast(int, opt.axis), cast(int, opt.amt), reduceop) for reduceop in self.reduceops]), "no tensor core available")
+      check(self._apply_tc_opt(use_tensor_cores, cast(int, opt.axis), cast(int, opt.amt)), "no tensor core available")
       self.applied_opts.append(opt)
       return
 
